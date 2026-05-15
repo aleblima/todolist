54b6df00f04b93fc1c0e8dcaa36c448e53a5cb55
Alejandro <alejandrolima1030@gmail.com>
2026-05-15 09:46:09 -0300

    chore: config prod environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://todolist-production-4203.up.railway.app'
+  apiUrl: 'https://todolist-production-4203.up.railway.app/api/tasks'
 };
